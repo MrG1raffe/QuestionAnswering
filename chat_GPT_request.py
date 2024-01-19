313c03d3df620fd851e3937ded97941042cac6cc
Дмитрий Сотников <dmitrysotni@gmail.com>
2024-01-19 13:45:56 +0100

    add ChatGPT request

---
+++ chat_GPT_request.py
@@ -0,0 +1,35 @@
+from openai import OpenAI
+
+
+def chat_gpt_request(
+    key: str,
+    question: str,
+    context: str = None
+):
+    client = OpenAI(api_key=key)
+
+    if context is not None:
+        request = f"""
+        Give concise answers without justifying and without making sentences, with the context that I give you. \n
+        Context: {context} \n
+        Question: {question}
+        """
+    else:
+        request = f"""
+        Give concise answers without justifying and without making sentences. \n
+        Question: {question}
+        """
+
+    print('Request:\n', request)
+
+    result = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": request,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+    return result.choices[0].message.content
